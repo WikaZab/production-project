76060cdf8a89f34f2205ff2a747205004c1b5597
Wikahome <radkovawika14@yandex.ru>
2024-11-13 15:21:49 +0300

    initial commit

---
+++ src/theme/useTheme.tsx
@@ -0,0 +1,18 @@
+import {LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext} from "./themeContext";
+import {useContext} from "react";
+
+interface useThemeResult{
+    toggleTheme: () => void;
+    theme: Theme;
+}
+
+
+export function useTheme(): useThemeResult {
+    const {theme, setTheme} = useContext(ThemeContext);
+    const toggleTheme = () => {
+        const newTheme = theme === Theme.Normal ? Theme.Dark : Theme.Normal;
+        setTheme(newTheme);
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
+    }
+    return {theme, toggleTheme}
+}
